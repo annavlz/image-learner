83f9edcc5d7fe405600694d696028e6f504ca252
Anna Lezhikova <Anna_Lezhikova@bnz.co.nz>
2018-09-10 18:33:47 +1200

    Add ts model warming up

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Webcam from 'react-webcam';
+import * as tf from '@tensorflow/tfjs';
+
+
+const loadMobilenet = async () => {
+  const mobilenet = await tf.loadModel('https://storage.googleapis.com/tfjs-models/tfjs/mobilenet_v1_0.25_224/model.json');
+  const layer = mobilenet.getLayer('conv_pw_13_relu');
+  return tf.model({ inputs: mobilenet.inputs, outputs: layer.output });
+}
+
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      image: null,
+      tab: 0
+    }
+  };
+
 
   setRef = (webcam) => {
     this.webcam = webcam;
   }
 
-  capture = () => {
-    this.imageSrc = this.webcam.getScreenshot();
-    console.log("GOT IT")
+  capture = async () => {
+    const {video, image} = this.webcam.getScreenshot();
+    this.setState({image})
+    const screenshot = tf.fromPixels(video).expandDims(0).toFloat().div(tf.scalar(127)).sub(tf.scalar(1));
+    const mobilenet = await loadMobilenet()
+    tf.tidy(() => mobilenet.predict(screenshot));
+    console.log("Mob", mobilenet)
   };
 
   render() {
@@ -20,10 +41,10 @@ class App extends Component {
         <header className="App-header">
           <div>
             <Webcam audio={false}
-              height={350}
+              height={224}
               ref={this.setRef}
               screenshotFormat="image/jpeg"
-              width={350}
+              width={224}
             />
             <button onClick={this.capture}>Capture photo</button>
           </div>
@@ -33,7 +54,7 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
         <div>
-          <img src={this.imageSrc} />
+          {this.state.image ? <img src={this.state.image} alt="" /> : null }
         </div>
       </div>
     );
