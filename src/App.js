97f03408740eed1cb77e46433f3f429c70ddc1b4
Anna Lezhikova <Anna_Lezhikova@bnz.co.nz>
2018-09-10 19:49:46 +1200

    Add new model loading

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 import Webcam from 'react-webcam';
 import * as tf from '@tensorflow/tfjs';
+import * as mobilenet from '@tensorflow-models/mobilenet';
 
-
-const loadMobilenet = async () => {
-  const mobilenet = await tf.loadModel('https://storage.googleapis.com/tfjs-models/tfjs/mobilenet_v1_0.25_224/model.json');
-  const layer = mobilenet.getLayer('conv_pw_13_relu');
-  return tf.model({ inputs: mobilenet.inputs, outputs: layer.output });
-}
+// const loadMobilenet = async () => {
+//   // const mobilenet = await tf.loadModel('https://storage.googleapis.com/tfjs-models/tfjs/mobilenet_v1_0.25_224/model.json');
+//   const model = await mobilenet.load()
+//   const layer = mobilenet.getLayer('conv_pw_13_relu');
+//   return tf.model({ inputs: model.inputs, outputs: layer.output });
+// }
 
 
 class App extends Component {
@@ -17,7 +18,8 @@ class App extends Component {
     super(props);
     this.state = {
       image: null,
-      tab: 0
+      tab: 0,
+      predictions: ''
     }
   };
 
@@ -28,19 +30,20 @@ class App extends Component {
 
   capture = async () => {
     const {video, image} = this.webcam.getScreenshot();
-    this.setState({image})
     const screenshot = tf.fromPixels(video).expandDims(0).toFloat().div(tf.scalar(127)).sub(tf.scalar(1));
-    const mobilenet = await loadMobilenet()
-    tf.tidy(() => mobilenet.predict(screenshot));
-    console.log("Mob", mobilenet)
+    const model = await mobilenet.load()
+    const predictions = tf.tidy(() => model.classify(screenshot));
+    this.setState({ image, predictions })
   };
 
   render() {
+    const {image, predictions} = this.state
     return (
       <div className="App">
         <header className="App-header">
           <div>
-            <Webcam audio={false}
+            <Webcam audio={true}
+              video={true}
               height={224}
               ref={this.setRef}
               screenshotFormat="image/jpeg"
@@ -54,7 +57,10 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
         <div>
-          {this.state.image ? <img src={this.state.image} alt="" /> : null }
+          {image ? <img src={image} alt="" /> : null }
+        </div>
+        <div>
+          {predictions ? predictions : "Nope"}
         </div>
       </div>
     );
